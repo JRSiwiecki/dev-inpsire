0252d1f91bed3d555db060e1780acdd42a86b10e
JRSiwiecki <JRSiwiecki@gmail.com>
2024-04-19 21:40:41 -0700

    Add home page and route to inspiration

---
+++ src/app/inspiration/page.tsx
@@ -0,0 +1,7 @@
+export default function Inspiration() {
+  return (
+    <main className="flex min-h-screen flex-col items-center bg-gray-800 p-5 text-white">
+      <h1 className="mb-4 text-6xl font-bold">Inspiration</h1>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
+import Link from "next/link";
+
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gray-800 text-white"></main>
+    <main className="flex min-h-screen flex-col items-center bg-gray-800 p-5 text-white">
+      <h1 className="mb-4 text-6xl font-bold">Dev Inspire</h1>
+      <p className="mb-8 text-lg ">
+        Get inspired with new and challenging project ideas for the next bullet
+        point on your resume!
+      </p>
+      <Link href="/inspiration">
+        <button className="rounded bg-blue-500 px-4 py-2 font-bold hover:bg-blue-600">
+          Get Started
+        </button>
+      </Link>
+    </main>
   );
 }
