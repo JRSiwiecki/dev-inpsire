58f10ba1b493c88503f1dc7ada9fcdb0c2d56b2b
JRSiwiecki <JRSiwiecki@gmail.com>
2024-04-19 21:52:10 -0700

    Style navbar

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -2,18 +2,30 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-800 p-4">
-      <div className="mx-auto flex max-w-7xl items-center justify-between">
+    <nav className="bg-gray-800">
+      <div className="container mx-auto flex items-center justify-between px-6 py-4">
         <div className="flex-shrink-0">
-          <Link href="/">Dev Inspire</Link>
+          <Link href="/">
+            <span className="cursor-pointer text-2xl font-bold text-white">
+              Dev Inspire
+            </span>
+          </Link>
         </div>
         <div className="hidden md:block">
           <ul className="flex space-x-4">
             <li>
-              <Link href="/">Home</Link>
+              <Link href="/">
+                <span className="cursor-pointer text-white hover:text-gray-300">
+                  Home
+                </span>
+              </Link>
             </li>
             <li>
-              <Link href="/inspiration">Inspiration</Link>
+              <Link href="/inspiration">
+                <span className="cursor-pointer text-white hover:text-gray-300">
+                  Inspiration
+                </span>
+              </Link>
             </li>
           </ul>
         </div>
