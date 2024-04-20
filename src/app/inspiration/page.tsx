8558e0f8cebadc99daf737562ee8a02e9dda17b9
JRSiwiecki <JRSiwiecki@gmail.com>
2024-04-20 11:39:12 -0700

    Add text input for inpsiration

--- src/app/inspiration/page.tsx
+++ src/app/inspiration/page.tsx
@@ -1,7 +1,50 @@
+"use client";
+
+import type { FormEvent } from "react";
+
 export default function Inspiration() {
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center bg-gray-800 p-5 text-white">
       <h1 className="mb-4 text-6xl font-bold">Inspiration</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="flex flex-col">
+          <div className="m-2 self-end">
+            <label htmlFor="position" className="mx-2">
+              Software Field/Position
+            </label>
+            <input
+              type="text"
+              name="position"
+              id="position"
+              className="text-black"
+            />
+          </div>
+          <div className="m-2 self-end">
+            <label htmlFor="topic" className="mx-2">
+              Topic within Field/Position
+            </label>
+            <input type="text" name="topic" id="topic" className="text-black" />
+          </div>
+          <div className="m-2 self-end">
+            <label htmlFor="topic" className="mx-2">
+              Technology in Field
+            </label>
+            <input
+              type="text"
+              name="technology"
+              id="technology"
+              className="text-black"
+            />
+          </div>
+          <button className="m-2 rounded bg-blue-500 px-4 py-2 font-bold hover:bg-blue-600">
+            Get Inspired
+          </button>
+        </div>
+      </form>
     </main>
   );
 }
