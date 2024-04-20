f24d768911f27108945ca467865fdb7da05a2d1c
JRSiwiecki <JRSiwiecki@gmail.com>
2024-04-20 16:56:49 -0700

    Add button, heading to generated inspiration

--- src/app/generated-inspiration/page.tsx
+++ src/app/generated-inspiration/page.tsx
@@ -16,7 +16,11 @@ export default function GeneratedInspiration() {
 
   return (
     <main className="flex min-h-screen flex-col items-center bg-gray-800 p-5 text-white">
-      <p className="max-w-lg">{response}</p>
+      <h1 className="bold text-6xl">Project Idea</h1>
+      <p className="m-4 max-w-lg">{response}</p>
+      <button className="m-2 rounded bg-blue-500 px-4 py-2 font-bold hover:bg-blue-600">
+        Save Idea
+      </button>
     </main>
   );
 }
