676aa14e0b73e58a2c8e07b9da20be314611d8da
JRSiwiecki <JRSiwiecki@gmail.com>
2024-04-20 16:52:34 -0700

    Update InspirationField to route to generated inspiration

--- src/app/_components/InspirationField.tsx
+++ src/app/_components/InspirationField.tsx
@@ -1,14 +1,25 @@
-import { type FormEvent } from "react";
-import { api } from "~/trpc/react";
+"use client";
+
+import { useState, type FormEvent } from "react";
 import Logout from "./Logout";
+import { useRouter } from "next/navigation";
 
 export default function InspirationField() {
-  const response = api.inspiration.generateInspiration.useQuery();
+  const router = useRouter();
+  const [position, setPosition] = useState("");
+  const [topic, setTopic] = useState("");
+  const [technology, setTechnology] = useState("");
 
   function handleSubmit(event: FormEvent) {
     event.preventDefault();
 
-    console.log(response);
+    if (position === "" || topic === "" || technology === "") {
+      return;
+    }
+
+    router.push(
+      `/generated-inspiration?position=${position}&topic=${topic}&technology=${technology}`,
+    );
   }
 
   return (
@@ -25,13 +36,20 @@ export default function InspirationField() {
               name="position"
               id="position"
               className="text-black"
+              onChange={(event) => setPosition(event.target.value)}
             />
           </div>
           <div className="m-2 self-end">
             <label htmlFor="topic" className="mx-2">
               Topic within Field/Position
             </label>
-            <input type="text" name="topic" id="topic" className="text-black" />
+            <input
+              type="text"
+              name="topic"
+              id="topic"
+              className="text-black"
+              onChange={(event) => setTopic(event.target.value)}
+            />
           </div>
           <div className="m-2 self-end">
             <label htmlFor="topic" className="mx-2">
@@ -42,6 +60,7 @@ export default function InspirationField() {
               name="technology"
               id="technology"
               className="text-black"
+              onChange={(event) => setTechnology(event.target.value)}
             />
           </div>
           <button className="m-2 rounded bg-blue-500 px-4 py-2 font-bold hover:bg-blue-600">
