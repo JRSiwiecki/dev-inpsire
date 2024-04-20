a98e686cdb97ad3b367f1f93cd7bf5a20e88e074
JRSiwiecki <JRSiwiecki@gmail.com>
2024-04-20 14:00:19 -0700

    Update login with text

--- src/app/_components/Login.tsx
+++ src/app/_components/Login.tsx
@@ -3,6 +3,7 @@ import { signIn } from "next-auth/react";
 export default function Login() {
   return (
     <>
+      <p className="m-2">To begin generating inspiration, please log in.</p>
       <button
         className="m-2 rounded bg-blue-500 px-4 py-2 font-bold hover:bg-blue-600"
         onClick={() => signIn()}
