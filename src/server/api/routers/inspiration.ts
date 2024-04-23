dd964907cd86b62aa1e1cb67d7b85773327e18f5
JRSiwiecki <JRSiwiecki@gmail.com>
2024-04-22 17:08:48 -0700

    Update types for router

--- src/server/api/routers/inspiration.ts
+++ src/server/api/routers/inspiration.ts
@@ -12,31 +12,35 @@ export const inspirationRouter = createTRPCRouter({
         technology: z.string(),
       }),
     )
-    .mutation(async (opts) => {
-      const openai = new OpenAI({
-        apiKey: env.OPENAI_API_KEY,
-      });
+    .mutation(
+      async (opts: {
+        input: { position: string; topic: string; technology: string };
+      }) => {
+        const openai = new OpenAI({
+          apiKey: env.OPENAI_API_KEY,
+        });
 
-      const gptResponse = await openai.chat.completions.create({
-        messages: [
-          {
-            role: "system",
-            content: `You are providing project ideas to people learning about or trying to 
+        const gptResponse = await openai.chat.completions.create({
+          messages: [
+            {
+              role: "system",
+              content: `You are providing project ideas to people learning about or trying to 
               get a job related to software.
               There should be a project idea, a description, a key features  with a 
               list of at least 5-10 features, and optional features with a list of 1-3 features.`,
-          },
-          {
-            role: "user",
-            content: `As an ${opts.input.position} with an interest in ${opts.input.topic}, 
+            },
+            {
+              role: "user",
+              content: `As an ${opts.input.position} with an interest in ${opts.input.topic}, 
             come up with a project idea where I can learn how to use ${opts.input.technology}.`,
-          },
-        ],
-        model: "gpt-3.5-turbo",
-      });
+            },
+          ],
+          model: "gpt-3.5-turbo",
+        });
 
-      return {
-        message: gptResponse.choices[0],
-      };
-    }),
+        return {
+          message: gptResponse.choices[0],
+        };
+      },
+    ),
 });
