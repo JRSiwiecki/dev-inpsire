b41dbb924e0e47455d1e8c262b33613fe4316263
JRSiwiecki <JRSiwiecki@gmail.com>
2024-04-25 15:41:06 -0700

    Update title for metadata

--- src/app/inspiration/[id]/page.tsx
+++ src/app/inspiration/[id]/page.tsx
@@ -25,7 +25,7 @@ export async function generateMetadata({ params }: { params: Params }) {
   });
 
   return {
-    title: user?.name,
+    title: `${user?.name}'s Inspirations`,
     description: `Saved inspirations for ${user?.name}`,
     openGraph: {
       title: user?.name,
