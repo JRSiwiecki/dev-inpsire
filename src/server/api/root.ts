f8c3a4e2a3588b389fb66b51459b40e837525530
JRSiwiecki <JRSiwiecki@gmail.com>
2024-04-25 15:31:15 -0700

    Remove unnecessary route

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,5 @@
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 import { inspirationRouter } from "./routers/inspiration";
-import { userRouter } from "./routers/users";
 
 /**
  * This is the primary router for your server.
@@ -9,7 +8,6 @@ import { userRouter } from "./routers/users";
  */
 export const appRouter = createTRPCRouter({
   inspiration: inspirationRouter,
-  users: userRouter,
 });
 
 // export type definition of API
