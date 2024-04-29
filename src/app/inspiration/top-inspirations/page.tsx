d620724918bdc660f0d1d9cbcaff9eda83c8367e
JRSiwiecki <JRSiwiecki@gmail.com>
2024-04-28 18:44:24 -0700

    Add top-inspirations page

---
+++ src/app/inspiration/top-inspirations/page.tsx
@@ -0,0 +1,41 @@
+import { type Inspiration } from "@prisma/client";
+import { db } from "~/server/db";
+
+export default async function TopInspirations() {
+  const inspirations = await db.inspiration.findMany();
+
+  async function findUser(userId: string) {
+    const user = await db.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    return user?.name;
+  }
+
+  return (
+    <main className="flex min-h-screen flex-col items-center bg-gray-800 p-5 text-white">
+      <h1 className="mb-4 text-6xl font-bold">Top Inspirations</h1>
+      <section className="flex max-w-2xl flex-col">
+        {inspirations.map((inspiration: Inspiration) => (
+          <div className="m-2 rounded-lg border-2 p-2" key={inspiration.id}>
+            <h3 className="text-2xl">
+              <b>
+                {inspiration.position}, {inspiration.topic},{" "}
+                {inspiration.technology}
+              </b>
+            </h3>
+            <h3 className="text-xl">
+              <b>{findUser(inspiration.userId)}</b>
+            </h3>
+            <h3>
+              <b>{inspiration.createdAt.toLocaleDateString()}</b>
+            </h3>
+            <p>{inspiration.savedInspiration}</p>
+          </div>
+        ))}
+      </section>
+    </main>
+  );
+}
